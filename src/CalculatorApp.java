b59eda6b9df817d40e2f6f9b3cfa251825d0e512
konekato <18ec033@ms.dendai.ac.jp>
2020-07-01 18:36:46 +0900

    fix: UI 変更

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -3,7 +3,6 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -30,6 +29,7 @@ public class CalculatorApp extends Application {
         // Create the Buttons
         Button[] btnN = new Button[10];
         for (int i = 0; i < btnN.length; i++) btnN[i] = new Button(Integer.toString(i));
+        Button btn00 = new Button("00");
         Button btnP = new Button(".");
         Button btnAdd = new Button("+");
         Button btnSub = new Button("-");
@@ -56,7 +56,7 @@ public class CalculatorApp extends Application {
         // Create the Layout
         HBox hbox1 = new HBox(5, tfQ, lb, tfA);
         hbox1.setAlignment(Pos.CENTER);
-        HBox hbox2 = new HBox(5, btnCE, btnAC);
+        HBox hbox2 = new HBox(5, btnCE, btnAC, btnEql);
         hbox2.setAlignment(Pos.CENTER);
         HBox hbox3 = new HBox(5, btnN[7], btnN[8], btnN[9], btnAdd);
         hbox3.setAlignment(Pos.CENTER);
@@ -64,7 +64,7 @@ public class CalculatorApp extends Application {
         hbox4.setAlignment(Pos.CENTER);
         HBox hbox5 = new HBox(5, btnN[1], btnN[2], btnN[3], btnMul);
         hbox5.setAlignment(Pos.CENTER);
-        HBox hbox6 = new HBox(5, btnN[0], btnP, btnEql, btnDiv);
+        HBox hbox6 = new HBox(5, btnN[0], btn00, btnP, btnDiv);
         hbox6.setAlignment(Pos.CENTER);
         VBox vbox = new VBox(20, hbox1, hbox2, hbox3, hbox4, hbox5, hbox6);
         vbox.setPadding(new Insets(20));
