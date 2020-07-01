337c26e79f8dbeeb6eed634be2e9e30b3058782e
konekato <18ec033@ms.dendai.ac.jp>
2020-07-01 23:18:16 +0900

    fix: func separate() に一部移行

--- src/sand.java
+++ src/sand.java
@@ -1,6 +1,11 @@
 
 
 public class sand {
+	static int[] priorityKey = new int[1000];
+	static int cnt = 0;
+	static int PKcnt = 0;
+	static int SYcnt = 0;
+	
 	static double calclateWithArithmeticOperation(String snum1, String snum2, String symbol) {
 		double num1 = 0;
 		double num2 = 0;
@@ -27,29 +32,15 @@ public class sand {
 		}
 		return ans;
 	}
-	/**
-	 * @param formula
-	 * @return
-	 */
-	static String calc(String formula) {
-		final String ERROR_MESSAGE = "Error.";
+	
+	// 数値と記号に分ける。小数点は数値と分類。
+	static String[] separate(String[] fArray) {
+		String[] sArray = new String[fArray.length];
 		
 		boolean isSymbol = false;
 		boolean isPoint = false;
-		String[] sArray = new String[formula.length()];
-		String[] fArray = formula.split("");
-		int[] priorityKey = new int[1000];
-		int cnt = 0;
-		int PKcnt = 0;
-		int SYcnt = 0;
 		
-		if (formula.length() == 0) {
-			System.out.println("数値が入力されていません。");
-			return ERROR_MESSAGE;
-		}
-		
-		// 数値と記号に分ける。小数点は数値と分類。
-		for (int i = 0; i < formula.length(); i++) {
+		for (int i = 0; i < fArray.length; i++) {
 			try {
 				Integer.parseInt(fArray[i]);
 				if (isSymbol) {
@@ -57,13 +48,13 @@ public class sand {
 					isSymbol = false;
 				}
 			} catch (Exception e) {
-				if (i <= 0 || i >= formula.length()-1) {
+				if (i <= 0 || i >= fArray.length-1) {
 					System.out.println("記号(+-*/.)がおかしなところにいるのでエラー: " + i);
-					return ERROR_MESSAGE;
+					return null;
 				}
 				if (isSymbol) {
 					System.out.println("記号(+-*/)が連続しているのでエラー: " + i);
-					return ERROR_MESSAGE;
+					return null;
 				}
 				switch (fArray[i]) {
 				case "×":
@@ -79,19 +70,41 @@ public class sand {
 				case ".":
 					if (isPoint) {
 						System.out.println("記号(.)が連続しているのでエラー: " + i);
-						return ERROR_MESSAGE;
+						return null;
 					}
 					else isPoint = true;
 					break;
 				default:
 					System.out.println("英字等はエラー: " + i);
-					return ERROR_MESSAGE;
+					return null;
 				}
 			}
 
 			if (sArray[cnt] == null) sArray[cnt] = fArray[i];
 			else sArray[cnt] += fArray[i];
 		}
+		
+		return sArray;
+	}
+	
+	
+	/**
+	 * @param formula
+	 * @return
+	 */
+	static String calc(String formula) {
+		final String ERROR_MESSAGE = "Error.";
+		
+		String[] sArray = new String[formula.length()];
+		String[] fArray = formula.split("");
+		
+		if (formula.length() == 0) {
+			System.out.println("数値が入力されていません。");
+			return ERROR_MESSAGE;
+		}
+		
+		sArray = separate(fArray);
+		if (sArray == null) return ERROR_MESSAGE;
 		
 		// debug
 		System.out.println("First");
@@ -130,6 +143,7 @@ public class sand {
 		System.out.println();
 		
 		for (int i = 0; i < SYcnt - PKcnt; i++) {
+			if (sArray[1] == null) break;
 			double ans = calclateWithArithmeticOperation(sArray[0], sArray[2], sArray[1]);
 			sArray[0] = Double.toString(ans);
 			for (int j = 1; j <= cnt-2; j++) {
@@ -143,11 +157,14 @@ public class sand {
 			System.out.println();
 			cnt -= 2;
 		}
+		if (sArray[0].endsWith(".0")) {
+			sArray[0] = sArray[0].substring(0, sArray[0].length() - 2);
+		}
 		
 		return sArray[0];
 	}
 	public static void main(String[] args) {
-		String str = "5.62 +1.38";
+		String str = "4÷2";
         System.out.println(str);
         System.out.println(calc(str));
     }
