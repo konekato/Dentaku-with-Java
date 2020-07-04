ca7df3fd7c04e14c4b080ee8b56f1ff4c4647891
konekato <18ec033@ms.dendai.ac.jp>
2020-07-04 17:26:53 +0900

    fix: test case 追加

--- src/CalculateTest.java
+++ src/CalculateTest.java
@@ -12,6 +12,7 @@ public class CalculateTest {
 				{"小数の除算", "1.1÷2.2", "0.5"},
 				{"小数点が数字の最後なら整数扱い", "11.+2.2", "13.2"},
 				{"初めに 0 がついている数の計算", "00001.1÷2.2", "0.5"},
+				{"数字のみ", "555.555", "555.555"},
 				{"複数の演算1", "1.1+2.2×2.2÷92123-32×77", "-2462.8999474615"},
 				{"複数の演算2", "2×3.141592×5×270÷360", "23.561940"},
 				{"除算で分母が 0 (エラー)", "1.1÷0", Calculate.ERROR_MESSAGE},
@@ -24,6 +25,7 @@ public class CalculateTest {
 				{"終わりに記号(+-×÷.)(エラー)", "1÷2+", Calculate.ERROR_MESSAGE},
 				{"式に空白が入っている(エラー)", "1 ÷ 2", Calculate.ERROR_MESSAGE},
 				{"式に記号(+-×÷.)以外の文字が入っている(エラー)", "x+2", Calculate.ERROR_MESSAGE},
+				{"記号のみ(エラー)", ".", Calculate.ERROR_MESSAGE},
 				{"入力値が空(エラー)", "", Calculate.ERROR_MESSAGE},
 		};
 		int cnt = 0;
@@ -32,7 +34,7 @@ public class CalculateTest {
 		for (int i = 0; i < testCases.length; i++) {
 			System.out.println("Case" + (i+1) + " " + testCases[i][0] + ":");
 			System.out.println("want " + testCases[i][1] + " = " + testCases[i][2]);
-			String ans = Calculate.calc(testCases[i][1]);
+			String ans = Calculate.calculate(testCases[i][1]);
 			if (ans.equals(testCases[i][2])) {
 				System.out.println(" -> 成功");
 				cnt++;
